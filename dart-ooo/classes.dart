f6c1aed8c96f748b7df69bf09914fecde271a5b3
sarincasm <eelsweb@gmail.com>
2023-08-21 16:46:50 +0200

    Dartpad 101: Functions

---
+++ dart-ooo/classes.dart
@@ -0,0 +1,17 @@
+void main() {
+  callMeMaybe(
+    String phone,
+    String areaCode,
+    String internationalCode,
+  ) {
+    print(phone);
+    print(areaCode);
+    print(internationalCode);
+  }
+
+  callMeMaybe(
+    '123',
+    '12',
+    '11',
+  );
+}
