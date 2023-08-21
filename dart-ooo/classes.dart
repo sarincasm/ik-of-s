0ff2a9f9dd280d441cda1eceb74ae2e9470e3f52
sarincasm <eelsweb@gmail.com>
2023-08-21 17:19:44 +0200

    Dart 101: Classes

--- dart-ooo/classes.dart
+++ dart-ooo/classes.dart
@@ -1,17 +1,30 @@
-void main() {
-  callMeMaybe({
+class Phone {
+  String phone = '';
+  String areaCode = '';
+  String internationalCode = '';
+
+  Phone({
     required String phone,
     required String areaCode,
     required String internationalCode,
   }) {
+    this.phone = phone;
+    this.areaCode = areaCode;
+    this.internationalCode = internationalCode;
+  }
+
+  printAll() {
     print(phone);
     print(areaCode);
     print(internationalCode);
   }
+}
 
-  callMeMaybe(
-    phone: '123',
-    areaCode: '12',
-    internationalCode: '11',
+void main() {
+  var phoneObject = Phone(
+    phone: '1234',
+    areaCode: '123',
+    internationalCode: '123',
   );
+  phoneObject.printAll();
 }
