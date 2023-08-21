cd564cbaec992831358dadee767f830283b5f074
sarincasm <eelsweb@gmail.com>
2023-08-21 18:56:23 +0200

    creator page backdrop

--- shoulders/lib/creator.dart
+++ shoulders/lib/creator.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Creator {
@@ -29,80 +31,98 @@ class CreatorProfile extends StatelessWidget {
     return Container(
       color: backgroundColor,
       width: double.infinity,
-      child: Column(children: [
-        const SizedBox(height: 120),
-        CircleAvatar(
-          radius: 75,
-          backgroundImage: NetworkImage(
-            creator.creatorImageUrl,
+      child: Container(
+        height: 550,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+              creator.creatorImageUrl,
+            ),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.5),
+              BlendMode.darken,
+            ),
           ),
         ),
-        const SizedBox(height: 30),
-        Text(
-          creator.creatorName,
-          style: const TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-        const SizedBox(height: 10),
-        Text(
-          creator.followerCount,
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-        const SizedBox(height: 30),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            OutlinedButton(
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(
-                side: const BorderSide(
-                  color: Colors.white,
-                  width: 1,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Follow'.toUpperCase(),
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 70, sigmaY: 60),
+          child: Column(children: [
+            const SizedBox(height: 120),
+            CircleAvatar(
+              radius: 75,
+              backgroundImage: NetworkImage(
+                creator.creatorImageUrl,
               ),
             ),
-            OutlinedButton(
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(
-                side: const BorderSide(
-                  color: Colors.white,
-                  width: 1,
-                ),
-                shape: const CircleBorder(),
+            const SizedBox(height: 30),
+            Text(
+              creator.creatorName,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
-              child: const Padding(
-                padding: EdgeInsets.all(4.0),
-                child: Icon(
-                  Icons.notification_add_outlined,
-                  color: Colors.white,
-                  size: 30,
-                ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              creator.followerCount,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
-            )
-          ],
+            ),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(
+                      color: Colors.white,
+                      width: 1,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Follow'.toUpperCase(),
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(
+                      color: Colors.white,
+                      width: 1,
+                    ),
+                    shape: const CircleBorder(),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(4.0),
+                    child: Icon(
+                      Icons.notification_add_outlined,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ]),
         ),
-      ]),
+      ),
     );
   }
 }
